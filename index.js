5a49f34b48edff237f991e26d9761be6c7a28000
Hepheir <hepheir@gmail.com>
2017-05-21 09:15:31 +0900

    back up before applying handlebars

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const port = 3000;
+
+const express = require('express');
+const fs = require('mz/fs');
+const handlebars = require('handlebars');
+
+const app = express();
+
+app.get(/([^/]*)(\/|\/index.html)$/, (req, res) => {
+    req.item = req.params[0];
+
+    let files,
+        handlebars_data = {
+            header__tabs: [
+                { font_icon: "create", desc: "글쓰기" },
+                { font_icon: "library_books", desc: "라이브러리" },
+                { font_icon: "explore", desc: "뉴스피드" }
+            ]
+        };
+
+    if ('partial' in req.query) {
+        files = fs.readFileSync(`app/${req.item}/index.html`);
+    } else {
+        files = [
+            fs.readFileSync('app/header.partial.html'),
+            fs.readFileSync(`app/${req.item}/index.html`),
+            fs.readFileSync('app/footer.partial.html')
+        ];
+    }
+
+    files.map(f => f.toString('utf-8'));
+    files.map(f => handlebars.compile(f));
+
+    const content = files.join('');
+
+    res.send(content);
+});
+
+app.use(express.static('app'));
+app.listen(port, () => {
+    console.log(`server listening to port ${port}`);
+});
