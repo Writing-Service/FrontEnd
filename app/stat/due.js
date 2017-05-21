a6a7c9a7cf71bb8fd7f9eb3d39c2c591f0250a5a
Hepheir <hepheir@gmail.com>
2017-05-21 20:58:46 +0900

    stable due date counter

--- app/stat/due.js
+++ app/stat/due.js
@@ -5,16 +5,57 @@ for (i = 1; document.getElementById('js--d-'+i) != undefined; i++) {
 }
 
 //미완성 이벤트
-window.setInterval(function(){
+window.setInterval( () => {
     let date = new Date();
 
-    target.map(function(t){
-        let due = t.getAttribute('dueto').split(':'),
-            timer = [
-                due[0] - date.getDate(),
-                due[1] - date.getHours(),
-                due[2] - date.getMinutes()
-            ];
-        t.innerHTML = `${timer[0]}D ${timer[1]}H ${timer[2]}M`;
+    target.map(t => {
+        let dueto = t.getAttribute('dueto').split(':'),
+            remain = {
+                dd :  dueto[0] - date.getDate(),
+                hh : dueto[1] - date.getHours(),
+                mm : dueto[2] - date.getMinutes(),
+                ss : dueto[3] - date.getSeconds()
+            };
+
+        if (remain.ss < 0) {
+            remain.ss += 60;
+            remain.mm -= 1;
+        }
+
+        if (remain.mm < 0) {
+            remain.mm += 60;
+            remain.hh -= 1;
+        }
+
+        if (remain.hh < 0) {
+            remain.hh += 24;
+            remain.dd -= 1;
+        }
+
+        if (remain.dd < 0) {
+            // 미완성 부분
+        }
+
+        let res = '';
+
+        //console.log(remain);
+
+        if (remain.dd < 0 && remain.hh <0 && remain.mm <0 && remain.ss < 0) {
+            res = 'DUE END';
+        } else if (remain.dd == remain.hh && remain.hh == 0) {
+            res = `${remain.mm}M ${remain.ss}S`;
+        } else {
+            if (remain.dd != 0)
+                res += `${remain.dd}D`;
+
+            if (remain.hh != 0)
+                res += ` ${remain.hh}H`;
+
+            if (remain.mm != 0)
+                res += ` ${remain.mm}M`;
+        }
+
+        if (t.innerHTML != res)
+            t.innerHTML = res;
     });
 },1000);
--- app/stat/test.js
+++ app/stat/test.js
@@ -9,23 +9,3 @@ document.body.addEventListener('click', () => {
 		isMobile = true;
 	}
 });
-
-var $types = document.querySelector('.js--types'),
-	$editor = document.querySelector('.js--editor'),
-	isOverTyped = false;
-
-function types(evt) {
-	let types = evt.target.value.length;
-	
-	$types.innerHTML = `${types}/700`;
-	
-	if (!isOverTyped && types > 700) {
-		$types.classList.add('types--warning');
-		isOverTyped = true;
-	}else if (isOverTyped && types <= 700) {
-		$types.classList.remove('types--warning');
-		isOverTyped = false;
-	}
-}
-
-$editor.addEventListener('keyup', types);
