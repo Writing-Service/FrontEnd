bf1ab780110e375394faa23d4882dc35c095c371
Hepheir <hepheir@gmail.com>
2017-06-23 04:17:03 +0900

    Enter키 뷰 모드 전환기능 - 삭제

--- 0.02/app/stat/test.js
+++ 0.02/app/stat/test.js
@@ -1,18 +1,5 @@
 var isMobile = false;
 
-document.body.addEventListener('keypress', function(evt) {
-	if (evt.keyCode != 13) // Enter
-		return;
-
-	if (isMobile) {
-		document.body.className = "desktop";
-		isMobile = false;
-	} else {
-		document.body.className = "mobile";
-		isMobile = true;
-	}
-});
-
 document.querySelector('.header__title').addEventListener('click', function(evt) {
 	if (isMobile) {
 		document.body.className = "desktop";
