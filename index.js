aadce1d059d61349088a68558d1b0296cd0fad5d
Hepheir <hepheir@gmail.com>
2017-08-06 17:23:34 +0900

    8/6 backend adaption

--- index.js
+++ index.js
@@ -144,6 +144,98 @@ function handlebarsContext(req, res){
                     saved: findObjectInArrayByKeyValue(user.docs.articles, 'thread_id', thread.thread_id).saved
                 };
             context.article = article;
+        
+        } else if (req.item == 'library' || req.item == 'newsfeed'){
+            let series = getSeriesJSON('j50384el'),
+                articles = [
+                    {
+                        id: series.series_id,
+                        author: series.articles[0].author_id,
+                        datetime: series.articles[0].datetime,
+                        date: '어제',
+                        content:series.articles[0].content,
+                        star: [
+                            {
+                                act: 1,
+                                satisfied: series.articles[0].satisfied
+                            },
+                            {
+                                act: 2,
+                                satisfied: series.articles[1].satisfied
+                            },
+                            {
+                                act: 3,
+                                satisfied: series.articles[2].satisfied
+                            },
+                            {
+                                act: 4,
+                                satisfied: series.articles[3].satisfied
+                            },
+                            {
+                                act: 5,
+                                satisfied: series.articles[4].satisfied
+                            },
+                            {
+                                act: 6,
+                                satisfied: series.articles[5].satisfied
+                            },
+                            {
+                                act: 7,
+                                satisfied: series.articles[6].satisfied
+                            }
+                        ],
+                        vote: {
+                            score: series.vote.up.length - series.vote.down.length,
+                            // admin id
+                            up: true,
+                            down: false,
+                        }
+                    },
+                    {
+                        id: 'asdfar12d',
+                        author: series.articles[1].author_id,
+                        datetime: series.articles[1].datetime,
+                        date: '내일',
+                        content: series.articles[1].content,
+                        star: [
+                            {
+                                act: 1,
+                                satisfied: false
+                            },
+                            {
+                                act: 2,
+                                satisfied: true
+                            },
+                            {
+                                act: 3,
+                                satisfied: true
+                            },
+                            {
+                                act: 4,
+                                satisfied: true
+                            },
+                            {
+                                act: 5,
+                                satisfied: true
+                            },
+                            {
+                                act: 6,
+                                satisfied: true
+                            },
+                            {
+                                act: 7,
+                                satisfied: false
+                            }
+                        ],
+                        vote: {
+                            score: series.vote.up.length - series.vote.down.length,
+                            // admin id
+                            up: true,
+                            down: false,
+                        }
+                    }
+                ];
+            context.articles = articles;
         }
 
         
--- noserver/stat/writing.js
+++ noserver/stat/writing.js
@@ -6,25 +6,13 @@
 
 
 var card = {
-        id: document.getElementsByClassName('js--card')[0].id
-    };
-
-    function getTimeKST(datetime) {
-        datetime = datetime.split(' ');
-
-        if (datetime[4] == 'KST') {
-            // Day Month Date HH:MM:SS KST YYYY -> Day Month Date YYYY HH:MM:SS GMT+0900 (KST)
-            datetime =  datetime[0] + ' ' + datetime[1] + ' ' + datetime[2] + ' ' + datetime[5] + ' ' + datetime[3] + ' GMT+0900 (KST)';
-        } else {
-            datetime = datetime.join(' ');
-        }
-        
-        return Date.parse(datetime);
-    }
+    id: document.getElementsByClassName('js--card')[0].id
+};
 
     card.timer = document.querySelector(`#${card.id} .js--timer`);
     card.date = new Date(
-        new Date(getTimeKST(document.querySelector('#' + card.id + ' .js--time').getAttribute('datetime')))
+        document.querySelector(`#${card.id} .js--time`)
+                .getAttribute('datetime')
     );
 
 
@@ -41,27 +29,27 @@ function updateTimer(card) {
     let timer = '';
 
     if (remainSecs >= 86400) {
-        timer += Math.floor(remainSecs/86400) + 'D';
+        timer += `${Math.floor(remainSecs/86400)}D`;
         remainSecs -= Math.floor(remainSecs/86400)*86400;
 
         if (remainSecs >= 3600) {
-            timer += ' ' + Math.floor(remainSecs/3600) + 'H';
+            timer += ` ${Math.floor(remainSecs/3600)}H`;
             remainSecs -= Math.floor(remainSecs/3600)*3600;
         }
         if (remainSecs >= 60) {
-            timer += ' ' + Math.floor(remainSecs/60) + 'M';
+            timer += ` ${Math.floor(remainSecs/60)}M`;
         }
     } else {
         if (remainSecs >= 3600) {
-            timer += Math.floor(remainSecs/3600) + 'H';
+            timer += ` ${Math.floor(remainSecs/3600)}H`;
             remainSecs -= Math.floor(remainSecs/3600)*3600;
         }
         if (remainSecs >= 60) {
-            timer += ' ' + Math.floor(remainSecs/60) + 'M';
+            timer += ` ${Math.floor(remainSecs/60)}M`;
             remainSecs -= Math.floor(remainSecs/60)*60;
         }
         if (remainSecs > 0) {
-            timer += ' ' + remainSecs + 'S';
+            timer += ` ${remainSecs}S`;
         }
     }
     
@@ -85,7 +73,7 @@ var editor = {
 function updateEditorTypes() {
     let types = editor.textarea.value.length;
 
-    editor.types.innerHTML = types + '/700';
+    editor.types.innerHTML = `${types}/700`;
     
     if (types > 700) {
         editor.types.classList.add('types--warn');
